3fadb5884edff66086bcad91a62b1258e326913f
Nishit Shah <ngshah3@myseneca.ca>
2023-01-07 14:22:15 -0500

    Add files via upload

---
+++ JavaScript/constant.js
@@ -0,0 +1,20 @@
+/*********************************************************************************
+ *  Author: Nishit Shah
+ *  Date: 2022-12-31
+ *  Title: Sudoku
+ *
+ *  I made this game for fun and to learn more about javascript.
+ *  I hope you enjoy it.
+ *  If you have any suggestions or feedback, please let me know.
+ *
+ *  Online URL:
+ ********************************************************************************/
+
+const CONSTANT = {
+  UNASSIGNED: 0,
+  GRID_SIZE: 9,
+  BOX_SIZE: 3,
+  NUMBERS: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  LEVEL_NAME: ["Easy", "Medium", "Hard", "Very hard", "Insane", "Inhuman"],
+  LEVEL: [29, 38, 47, 56, 65, 74],
+};
